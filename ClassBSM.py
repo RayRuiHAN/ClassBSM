c6566a24d3ffff66269a1a5c437f4e8dab2d0504
Ray(Rui) HAN <rayhan1317.cn@gmail.com>
2022-04-08 22:20:48 +0800

    Update ClassBSM.py

--- ClassBSM.py
+++ ClassBSM.py
@@ -5,7 +5,7 @@ from scipy.stats import norm
 
 class BSM(object):
 
-    def __init__(self, asset_price, exercise_price, annualized_remaining, annualized_sigma, rf_rate):
+    def __init__(self, asset_price, exercise_price, remaining, sigma, rf_rate):
         """
         asset_price: self.S
         exercise_price: self.K
@@ -16,8 +16,8 @@ class BSM(object):
         """
         self.S = asset_price
         self.K = exercise_price
-        self.T = annualized_remaining
-        self.q = annualized_sigma
+        self.T = remaining
+        self.q = sigma
         self.r = rf_rate
         self.d1 = (math.log(self.S / self.K) + (self.r + 0.5 * self.q * self.q) * self.T) / (
                 self.q * math.sqrt(self.T))
@@ -29,7 +29,7 @@ class BSM(object):
 
 
 if __name__ == '__main__':
-    bsm = BSM(4815.226, 4500, 0.0877, 0.2643, 0.01)
+    bsm = BSM(asset_price=4815.226, exercise_price=4500, remaining=0.0877, sigma=0.2643, rf_rate=0.01)
     print(bsm.d1)
     print(bsm.d2)
     print(bsm.CallPrice)
