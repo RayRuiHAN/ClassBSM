bfdcd890eb932d2528a67df7d3c1706182217f30
Ray HAN <rayhan1317.cn@gmail.com>
2023-03-12 19:04:34 -0400

    Update ClassBSM.py

--- ClassBSM.py
+++ ClassBSM.py
@@ -1,11 +1,11 @@
-# BS function
+# BSM Solver including Greeks, using the Newton-Raphson iterative algorithm
 import math
 from scipy.stats import norm
 
 
-class BSM(object):
+class bsm_solver(object):
 
-    def __init__(self, asset_price, exercise_price, remaining, call_put='C', sigma=0.5, rf_rate=0.1):
+    def __init__(self, asset_price, exercise_price, remaining, call_put=1, sigma=0.5, rf_rate=0.1):
         """
         asset_price: self.S
         exercise_price: self.K
@@ -26,7 +26,7 @@ class BSM(object):
         self.Gamma = norm.pdf(self.d1) / (self.S * self.q * math.sqrt(self.T))
         self.Vega = self.S * norm.pdf(self.d1) * math.sqrt(self.T)
 
-        if call_put == 'C':
+        if call_put == 1:
             self.Price = norm.cdf(self.d1) * self.S - norm.cdf(self.d2) * self.K * math.exp(-self.r * self.T)
             self.Delta = norm.cdf(self.d1)
             self.Theta = -1 * (self.S * norm.pdf(self.d1) * self.q) / (
@@ -40,7 +40,7 @@ class BSM(object):
                 2 * math.sqrt(self.T)) + self.r * self.K * math.exp(-self.r * self.T) * norm.cdf(-1 * self.d2)
             self.Rho = -1 * self.T * self.K * math.exp(-self.r * self.T) * norm.cdf(-1 * self.d2)
 
-    def vol(self, target_price, max_iterations=200, precision=1.0e-5):
+    def vol(self, target_price, max_iterations=500, precision=1.0e-5):
         q = 0.5
         call_put = self.call_put
         for i in range(0, max_iterations):
@@ -60,7 +60,7 @@ class BSM(object):
 
 
 if __name__ == '__main__':
-    bsm = BSM(asset_price=4815.226, exercise_price=4500, remaining=0.0877, call_put='C', sigma=0.2643, rf_rate=0)
+    bsm = bsm_solver(asset_price=4815.226, exercise_price=4500, remaining=0.0877, call_put='C', sigma=0.2643, rf_rate=0)
     print(bsm.d1)
     print(bsm.d2)
     print(bsm.Price)
@@ -70,4 +70,4 @@ if __name__ == '__main__':
     print(bsm.Vega)
     print(bsm.Rho)
     print(bsm.vol(target_price=354.205))
-    print(BSM(asset_price=4815.226, exercise_price=4500, remaining=0.0877, call_put='C', rf_rate=0).vol(target_price=354.205))
+    print(BSM(asset_price=4815.226, exercise_price=4500, remaining=0.0877, call_put=1, rf_rate=0).vol(target_price=354.205))
